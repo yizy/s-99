aa8615efb3925152cd4d5dc6875ab8ebdc999ca3
yizy <zy473@nyu.edu>
2017-07-28 23:40:28 -0700

    Update P01.scala

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,4 +1,11 @@
-// http://aperiodic.net/phil/scala/s-99/p01.scala
+/*
+	P01 (*) Find the last element of a list.
+	Example:
+	scala> last(List(1, 1, 2, 3, 5, 8))
+	res0: Int = 8
+
+	http://aperiodic.net/phil/scala/s-99/p01.scala
+*/
 
 object P01 {
 	def last[A](lst: List[A]): A = lst match {
