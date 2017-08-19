7809e69a00b2b7c9f66306e6c3128e9dbd37a6a5
yizy <zy473@nyu.edu>
2017-08-18 23:27:50 -0700

    Create P05.scala

---
+++ src/main/scala/P05.scala
@@ -0,0 +1,25 @@
+/*
+  P05 (*) Reverse a list.
+  Example:
+  scala> reverse(List(1, 1, 2, 3, 5, 8))
+  res0: List[Int] = List(8, 5, 3, 2, 1, 1)
+  
+  http://aperiodic.net/phil/scala/s-99/p05.scala
+*/
+
+import scala.annotation.tailrec
+
+object P05 {
+	def reverseSimple[A](lst: List[A]): List[A] = lst match {
+		case h::tail => reverseSimple(tail) ::: List(h)
+		case Nil => Nil
+	}
+
+	@tailrec
+	def reverseTailRec[A](lst: List[A], res: List[A] = Nil): List[A] = lst match {
+		case h::tail => reverseTailRec(tail, h :: res)
+		case Nil => res
+	}
+
+	def reverseByFold[A](lst: List[A]): List[A] = lst.foldLeft(List[A]()) { (b, a) => a :: b}
+}
