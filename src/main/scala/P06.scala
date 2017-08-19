95b637966b7b9d9a8d90b25669f931a29f960396
yizy <zy473@nyu.edu>
2017-08-19 16:03:15 -0700

    Create P06.scala

---
+++ src/main/scala/P06.scala
@@ -0,0 +1,14 @@
+/*
+  P06 (*) Find out whether a list is a palindrome.
+  Example:
+  scala> isPalindrome(List(1, 2, 3, 2, 1))
+  res0: Boolean = true
+	
+  http://aperiodic.net/phil/scala/s-99/p06.scala
+*/
+
+import scala.annotation.tailrec
+
+object P06 {
+	def isPalindrome[A](lst: List[A]): Boolean = lst == lst.reverse
+}
