10ca2be86220f01dfd4ac70889015769d8f8e8b7
yizy <zy473@nyu.edu>
2017-08-19 21:30:17 -0700

    Update P04.scala

--- src/main/scala/P04.scala
+++ src/main/scala/P04.scala
@@ -10,15 +10,15 @@
 import scala.annotation.tailrec
 
 object P04 {
-	def lengthSimple[A](lst: List[A]): Int = lst match {
-		case _::tail => 1 + lengthSimple(tail)
-		case Nil => 0
+  def lengthSimple[A](lst: List[A]): Int = lst match {
+    case _ :: tail => 1 + lengthSimple(tail)
+    case Nil       => 0
 	}
 
 	@tailrec
 	def lengthTailRec[A](lst: List[A], n: Int = 0): Int = lst match {
-		case _::tail => lengthTailRec(tail, n + 1)
-		case Nil => n
+    case _ :: tail => lengthTailRec(tail, n + 1)
+    case Nil       => n
 	}
 
 	def lengthByFold[A](lst: List[A]): Int = lst.foldLeft(0) { (b, _) => b + 1}
