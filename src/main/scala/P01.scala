9d0bfe98a09da8f54bceb7ad4b0fb82fd161653a
yizy <zy473@nyu.edu>
2017-08-19 21:28:28 -0700

    Update P01.scala

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,16 +1,16 @@
 /*
-	P01 (*) Find the last element of a list.
-	Example:
-	scala> last(List(1, 1, 2, 3, 5, 8))
-	res0: Int = 8
+  P01 (*) Find the last element of a list.
+  Example:
+  scala> last(List(1, 1, 2, 3, 5, 8))
+  res0: Int = 8
 
-	http://aperiodic.net/phil/scala/s-99/p01.scala
+  http://aperiodic.net/phil/scala/s-99/p01.scala
 */
 
 object P01 {
-	def last[A](lst: List[A]): A = lst match {
-		case h :: Nil  => h
-		case _ :: tail => last(tail)
-		case _ 		     => throw new NoSuchElementException
-	}
+  def last[A](lst: List[A]): A = lst match {
+    case h :: Nil  => h
+    case _ :: tail => last(tail)
+    case _ 		     => throw new NoSuchElementException
+  }
 }
