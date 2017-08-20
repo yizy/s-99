ed3b6f14bf223b8673ec019808f100a87013c79e
yizy <zy473@nyu.edu>
2017-08-19 21:30:41 -0700

    Update P04.scala

--- src/main/scala/P04.scala
+++ src/main/scala/P04.scala
@@ -13,13 +13,13 @@ object P04 {
   def lengthSimple[A](lst: List[A]): Int = lst match {
     case _ :: tail => 1 + lengthSimple(tail)
     case Nil       => 0
-	}
+  }
 
 	@tailrec
 	def lengthTailRec[A](lst: List[A], n: Int = 0): Int = lst match {
     case _ :: tail => lengthTailRec(tail, n + 1)
     case Nil       => n
-	}
+  }
 
 	def lengthByFold[A](lst: List[A]): Int = lst.foldLeft(0) { (b, _) => b + 1}
 }
