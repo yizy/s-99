d9a641effb3ba3689843a0e18a969c639df1c858
yizy <zy473@nyu.edu>
2017-08-19 21:57:24 -0700

    Create P09.scala

---
+++ src/main/scala/P09.scala
@@ -0,0 +1,21 @@
+/*
+  P09 (**) Pack consecutive duplicates of list elements into sublists.
+  If a list contains repeated elements they should be placed in separate sublists.
+  Example:
+
+  scala> pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+  res0: List[List[Symbol]] = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+	
+  http://aperiodic.net/phil/scala/s-99/p09.scala
+ */
+
+import scala.annotation.tailrec
+
+object P09 {
+  @tailrec
+  def pack(lst: List[Any], tmp: List[Any] = Nil, res: List[Any] = Nil): List[Any] = lst match {
+    case h :: tail if tmp.isEmpty || tmp.head == h => pack(tail, h :: tmp, res)
+    case h :: tail => pack(tail, List(h), tmp :: res)
+    case Nil => (tmp :: res).reverse
+  }
+}
