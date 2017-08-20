fb578cfa323b830fe6734fda886c346f1783dd2a
yizy <zy473@nyu.edu>
2017-08-19 21:26:10 -0700

    Update P01.scala

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -9,8 +9,8 @@
 
 object P01 {
 	def last[A](lst: List[A]): A = lst match {
-		case h::Nil  => h
-		case _::tail => last(tail)
-		case _ 		   => throw new NoSuchElementException
+		case h :: Nil  => h
+		case _ :: tail => last(tail)
+		case _ 		     => throw new NoSuchElementException
 	}
 }
